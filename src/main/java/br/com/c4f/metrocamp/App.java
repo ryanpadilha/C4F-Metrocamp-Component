d788d21f1fb33de375db14537760cd465e1f1345
Ryan Padilha <ryan.padilha@gmail.com>
2016-03-19 13:53:32 -0300

    initial commit

---
+++ src/main/java/br/com/c4f/metrocamp/App.java
@@ -0,0 +1,13 @@
+package br.com.c4f.metrocamp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
